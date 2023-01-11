036915e5e5bc52e17d429fd9f678c679d018a9b5
zDany01 <85403430+zDany01@users.noreply.github.com>
2023-01-11 01:07:59 +0100

    Fixed ContextMenu for all elements
    Changed the main container from an internal one to a panel to inherit the context menu
    Created the context menu before the buttons to make them inherit it
    Removed mouse listener events and interface
    Removed useless imports

--- IntelliJ/Java/src/GUI.java
+++ IntelliJ/Java/src/GUI.java
@@ -2,11 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
-public class GUI extends JDialog implements MouseListener, ActionListener {
+public class GUI extends JDialog implements ActionListener {
     private final Dimension defaultButtonSize = new Dimension(40,36);
     private List<JButton> buttonGrid = new ArrayList();
     public GUI(){
@@ -15,24 +13,9 @@ public class GUI extends JDialog implements MouseListener, ActionListener {
 
     //Events
     @Override
-    public void mouseClicked(MouseEvent e) {
-        if(SwingUtilities.isRightMouseButton(e)) contextMenu.show(this, e.getX(), e.getY());
-    }
-    @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == exitMenuItem) this.dispose();
     }
-
-
-    @Override
-    public void mousePressed(MouseEvent e) {}
-    @Override
-    public void mouseReleased(MouseEvent e){}
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-    @Override
-    public void mouseExited(MouseEvent e) { }
-
 
 
     //Graphics
@@ -44,10 +27,15 @@ public class GUI extends JDialog implements MouseListener, ActionListener {
         this.setSize(202,189);
         this.setResizable(false);
         this.setTitle("TicTacToe");
-        this.setLayout(new FlowLayout(FlowLayout.CENTER,20,0));
-        this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        this.addMouseListener(this);
-        this.getContentPane().add(Box.createRigidArea(new Dimension(this.getWidth(),13)));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setSize(this.getSize());
+        this.add(mainPanel);
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER,20,0));
+        mainPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        mainPanel.add(Box.createRigidArea(new Dimension(this.getWidth(),13)));
+        contextMenu = new JPopupMenu();
+        mainPanel.setComponentPopupMenu(contextMenu);
+
         buttonGrid.add(button1);
         buttonGrid.add(button2);
         buttonGrid.add(button3);
@@ -61,10 +49,10 @@ public class GUI extends JDialog implements MouseListener, ActionListener {
             buttonGrid.set(i, new JButton());
             buttonGrid.get(i).setPreferredSize(defaultButtonSize);
             buttonGrid.get(i).setBackground(Color.white);
-            this.getContentPane().add(buttonGrid.get(i));
-            if(i == 2 || i == 5) this.getContentPane().add(Box.createRigidArea(new Dimension(this.getWidth(),8)));
+            buttonGrid.get(i).setInheritsPopupMenu(true);
+            mainPanel.add(buttonGrid.get(i));
+            if(i == 2 || i == 5) mainPanel.add(Box.createRigidArea(new Dimension(this.getWidth(),8)));
         }
-        contextMenu = new JPopupMenu();
         restartMenuItem = new JMenuItem("Restart");
         exitMenuItem = new JMenuItem("Exit");
         restartMenuItem.addActionListener(this);
--- IntelliJ/Java/src/Program.java
+++ IntelliJ/Java/src/Program.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 public class Program {
     public static void main(String[] args){
         new GUI().setVisible(true);
