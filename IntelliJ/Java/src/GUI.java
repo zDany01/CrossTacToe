5568d37793acd6c84451ee1038ad230ec10738af
zDany01 <85403430+zDany01@users.noreply.github.com>
2023-01-12 23:38:43 +0100

    Optimized Code
    Removed useless button declaration
    Made rigid area divider dimension constant

--- IntelliJ/Java/src/GUI.java
+++ IntelliJ/Java/src/GUI.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class GUI extends JDialog implements ActionListener {
     private final Dimension defaultButtonSize = new Dimension(40,36);
+    private final Dimension defaultDividerSize = new Dimension(202, 8);
     private List<JButton> buttonGrid = new ArrayList();
     public GUI(){
         InitializeComponent();
@@ -22,7 +23,6 @@ public class GUI extends JDialog implements ActionListener {
     }
     //</editor-fold>
     //<editor-fold desc="Graphics">
-    private JButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
     private JPopupMenu contextMenu;
     private JMenuItem restartMenuItem, exitMenuItem;
     private void InitializeComponent(){
@@ -39,22 +39,14 @@ public class GUI extends JDialog implements ActionListener {
         mainPanel.add(Box.createRigidArea(new Dimension(this.getWidth(),13)));
         contextMenu = new JPopupMenu();
         mainPanel.setComponentPopupMenu(contextMenu);
-        buttonGrid.add(button1);
-        buttonGrid.add(button2);
-        buttonGrid.add(button3);
-        buttonGrid.add(button4);
-        buttonGrid.add(button5);
-        buttonGrid.add(button6);
-        buttonGrid.add(button7);
-        buttonGrid.add(button8);
-        buttonGrid.add(button9);
-        for(int i = 0; i < buttonGrid.size(); i++){
-            buttonGrid.set(i, new JButton());
-            buttonGrid.get(i).setPreferredSize(defaultButtonSize);
-            buttonGrid.get(i).setBackground(Color.white);
-            buttonGrid.get(i).setInheritsPopupMenu(true);
-            mainPanel.add(buttonGrid.get(i));
-            if(i == 2 || i == 5) mainPanel.add(Box.createRigidArea(new Dimension(this.getWidth(),8)));
+        for(int i = 0; i < 9; i++){
+            JButton button = new JButton();
+            button.setPreferredSize(defaultButtonSize);
+            button.setBackground(Color.white);
+            button.setInheritsPopupMenu(true);
+            buttonGrid.add(button);
+            mainPanel.add(button);
+            if(i == 2 || i == 5) mainPanel.add(Box.createRigidArea(defaultDividerSize));
         }
         restartMenuItem = new JMenuItem("Restart");
         exitMenuItem = new JMenuItem("Exit");
