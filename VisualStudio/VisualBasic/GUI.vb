8bfc188dc90dc9e422be7efe7a9e6b9c775222e8
zDany01 <85403430+zDany01@users.noreply.github.com>
2023-01-17 00:18:28 +0100

    Fixed && Optimized VisualBasic Code
    Fixed GameLogic not being called on clicks
    Fixed Winning Messagebox icon
    Added missing ByVal attribute
    
    Added accessibility modifier
    Changed buttonGrid from a List to an Array
    Used ResetText function instead of assigning text to a zero-lenght string
    Checking to string.empty instead of zero-lenght string
    Removed useless import

--- VisualStudio/VisualBasic/GUI.vb
+++ VisualStudio/VisualBasic/GUI.vb
@@ -1,35 +1,27 @@
 ﻿Public Class GUI
-    Const APP_NAME As String = "Tic Tac Toe"
+    Private Const APP_NAME As String = "Tic Tac Toe"
     Private clickCount As Integer = 0
     Private gameEnded As Boolean = False
-    Private buttonGrid As List(Of Button) = New List(Of Button)
+    Private buttonGrid(9) As Button
 
-    Private Sub GUI_Load(sender As Object, e As EventArgs) Handles Me.Load
-        buttonGrid.Add(button1)
-        buttonGrid.Add(button2)
-        buttonGrid.Add(button3)
-        buttonGrid.Add(button4)
-        buttonGrid.Add(button5)
-        buttonGrid.Add(button6)
-        buttonGrid.Add(button7)
-        buttonGrid.Add(button8)
-        buttonGrid.Add(button9)
+    Private Sub GUI_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        buttonGrid = New Button() {button1, button2, button3, button4, button5, button6, button7, button8, button9}
     End Sub
 
     Private Sub ResetGame()
         For Each button As Button In buttonGrid
-            button.Text = ""
+            button.ResetText()
             button.ForeColor = Button.DefaultForeColor
         Next
         gameEnded = False
         clickCount = 0
     End Sub
-    Private Sub FinishGame(pattern As (Button, Button, Button))
+    Private Sub FinishGame(ByVal pattern As (Button, Button, Button))
         pattern.Item1.ForeColor = Color.Green
         pattern.Item2.ForeColor = Color.Green
         pattern.Item3.ForeColor = Color.Green
         gameEnded = True
-        If MsgBox($"Player {IIf(pattern.Item2.Text = "X", "one", "two")} has won!", MsgBoxStyle.RetryCancel, APP_NAME) = MsgBoxResult.Retry Then ResetGame()
+        If MsgBox($"Player {IIf(pattern.Item2.Text = "X", "one", "two")} has won!", MsgBoxStyle.RetryCancel + MsgBoxStyle.Information, APP_NAME) = MsgBoxResult.Retry Then ResetGame()
     End Sub
     Private Sub ExitToolStripMenu(ByVal sender As Object, ByVal e As EventArgs) Handles exitToolStripMenuItem.Click
         Application.Exit()
@@ -37,24 +29,24 @@
     Private Sub RestartToolStripMenu(ByVal sender As Object, ByVal e As EventArgs) Handles restartToolStripMenuItem.Click
         If MsgBox("Are you sure?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, APP_NAME) = MsgBoxResult.Yes Then ResetGame()
     End Sub
-    Private Sub GameLogic(ByVal sender As Object, ByVal e As EventArgs) 
+    Private Sub GameLogic(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click, button2.Click, button3.Click, button4.Click, button5.Click, button6.Click, button7.Click, button8.Click, button9.Click
         If gameEnded Then Return
         Dim currentButton As Button = CType(sender, Button)
-        If currentButton.Text = "" Then
+        If currentButton.Text = String.Empty Then
             currentButton.Text = IIf(clickCount Mod 2 = 0, "X", "O")
             clickCount += 1
 
             For i As Integer = 1 To buttonGrid.Count - 1 Step +3
-                If (buttonGrid.Item(i - 1).Text = "X" AndAlso buttonGrid.Item(i).Text = "X" AndAlso buttonGrid.Item(i + 1).Text = "X") OrElse (buttonGrid.Item(i - 1).Text = "O" AndAlso buttonGrid.Item(i).Text = "O" AndAlso buttonGrid.Item(i + 1).Text = "O") Then FinishGame((buttonGrid.Item(i - 1), buttonGrid.Item(i), buttonGrid.Item(i + 1)))
+                If (buttonGrid(i - 1).Text = "X" AndAlso buttonGrid(i).Text = "X" AndAlso buttonGrid(i + 1).Text = "X") OrElse (buttonGrid(i - 1).Text = "O" AndAlso buttonGrid(i).Text = "O" AndAlso buttonGrid(i + 1).Text = "O") Then FinishGame((buttonGrid(i - 1), buttonGrid(i), buttonGrid(i + 1)))
             Next
             For i As Integer = 3 To 5
-                If (buttonGrid.Item(i - 3).Text = "X" AndAlso buttonGrid.Item(i).Text = "X" AndAlso buttonGrid.Item(i + 3).Text = "X") OrElse (buttonGrid.Item(i - 3).Text = "O" AndAlso buttonGrid.Item(i).Text = "O" AndAlso buttonGrid.Item(i + 3).Text = "O") Then FinishGame((buttonGrid.Item(i - 3), buttonGrid.Item(i), buttonGrid.Item(i + 3)))
+                If (buttonGrid(i - 3).Text = "X" AndAlso buttonGrid(i).Text = "X" AndAlso buttonGrid(i + 3).Text = "X") OrElse (buttonGrid(i - 3).Text = "O" AndAlso buttonGrid(i).Text = "O" AndAlso buttonGrid(i + 3).Text = "O") Then FinishGame((buttonGrid(i - 3), buttonGrid(i), buttonGrid(i + 3)))
             Next
-            If (buttonGrid.Item(0).Text = "X" AndAlso buttonGrid.Item(4).Text = "X" AndAlso buttonGrid.Item(8).Text = "X") OrElse (buttonGrid.Item(0).Text = "O" AndAlso buttonGrid.Item(4).Text = "O" AndAlso buttonGrid.Item(8).Text = "O") Then
-                FinishGame((buttonGrid.Item(0), buttonGrid.Item(4), buttonGrid.Item(8)))
-            ElseIf (buttonGrid.Item(2).Text = "X" AndAlso buttonGrid.Item(4).Text = "X" AndAlso buttonGrid.Item(6).Text = "X") OrElse (buttonGrid.Item(2).Text = "O" AndAlso buttonGrid.Item(4).Text = "O" AndAlso buttonGrid.Item(6).Text = "O") Then
-                FinishGame((buttonGrid.Item(2), buttonGrid.Item(4), buttonGrid.Item(6)))
-            ElseIf clickCount = 9 AndAlso MsgBox("Draw", MsgboxStyle.Information + MsgBoxStyle.RetryCancel, APP_NAME) = MsgBoxResult.Retry Then
+            If (buttonGrid(0).Text = "X" AndAlso buttonGrid(4).Text = "X" AndAlso buttonGrid(8).Text = "X") OrElse (buttonGrid(0).Text = "O" AndAlso buttonGrid(4).Text = "O" AndAlso buttonGrid(8).Text = "O") Then
+                FinishGame((buttonGrid(0), buttonGrid(4), buttonGrid(8)))
+            ElseIf (buttonGrid(2).Text = "X" AndAlso buttonGrid(4).Text = "X" AndAlso buttonGrid(6).Text = "X") OrElse (buttonGrid(2).Text = "O" AndAlso buttonGrid(4).Text = "O" AndAlso buttonGrid(6).Text = "O") Then
+                FinishGame((buttonGrid(2), buttonGrid(4), buttonGrid(6)))
+            ElseIf clickCount = 9 AndAlso MsgBox("Draw", MsgBoxStyle.Information + MsgBoxStyle.RetryCancel, APP_NAME) = MsgBoxResult.Retry Then
                 ResetGame()
             End If
         Else
